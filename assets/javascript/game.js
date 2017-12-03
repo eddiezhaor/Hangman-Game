ff7edc7a145dac3674412b75b1aecadfdc5306b3
eddiezhaor <eddiezhaor@gmail.com>
2017-12-03 12:41:48 -0600

    third

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 var win=0;
-var images=["assets/images/cat.jpg","assets/images/cat2.jpg","assets/images/flower.jpg","assets/images/wolf.jpg","assets/images/cool.jpg","assets/images/dog.jpeg","assets/images/funny1.jpeg","assets/images/exam.jpg"];
-var num = Math.floor(Math.random() * images.length)
+var images=["assets/images/cat.jpg","assets/images/cat2.jpeg","assets/images/cat3.jpeg","assets/images/cat4.jpg"];
+var num=Math.floor(Math.random()*images.length);
 function start(){
 var words=["addition",    "adjustment",    "advertisement",    "after",    "again",    "against",    "agreement",    "air",    "all",    "almost",    "among",    "amount",    "amusement",    "and",    "angle",    "angry",    "animal",    "answer",    "ant",    "any",    "apparatus",    "apple",    "approval",    "arch",    "argument",    "arm",    "army",    "art",    "as",    "at",    "attack",    "attempt",    "attention",    "attraction",    "authority",    "automatic",    "awake",    "baby",    "back",    "bad",    "bag",    "balance",    "ball",    "band",    "base",    "basin",    "basket",    "bath",    "be",    "beautiful",    "because",    "bed",    "bee",    "before",    "behaviour",    "belief",    "bell",    "bent",    "berry",    "between",    "bird",    "birth",    "bit",    "bite",    "bitter",    "black",    "blade",    "blood",    "blow",    "blue",    "board",    "boat",    "body",    "boiling",    "bone",    "book",    "boot",    "bottle",    "box",    "boy",    "brain",    "brake",    "branch",    "brass",    "bread",    "breath",    "brick",    "bridge",    "bright",    "broken",    "brother",    "brown",    "brush",    "bucket",    "building",    "bulb",    "burn",    "burst",    "business",    "but",    "butter",    "button",    "by",    "cake",    "camera",    "canvas",    "card",    "care",    "carriage",    "cart",    "cat",    "cause",    "certain",    "chain",    "chalk",    "chance",    "change",    "cheap",    "cheese",    "chemical",    "chest",    "chief",    "chin",    "church",    "circle",    "clean",    "clear",    "clock",    "cloth",    "cloud",    "coal",    "coat",    "cold",    "collar",    "colour",    "comb",    "come",    "comfort",    "committee",    "common",    "company",    "comparison",    "competition",    "complete",    "complex",    "condition",    "connection",    "conscious",    "control",    "cook",    "copper",    "copy",    "cord",    "cork",    "cotton",    "cough",    "country",    "cover",    "cow",    "crack",    "credit",    "crime",    "cruel",    "crush",    "cry",    "cup",    "cup",    "current",    "curtain",    "curve",    "cushion",    "damage",    "danger",    "dark",    "daughter",    "day",    "dead",    "dear",    "death",    "debt",    "decision",    "deep",    "degree",    "delicate",    "dependent",    "design",    "desire",    "destruction",    "detail",    "development",    "different",    "digestion"];
 var remaining=12;
@@ -88,6 +88,6 @@ function stop(){
 	start();
 };
 function displayimg(){
-	document.getElementById("myimg").innerHTML=`<img src="${images[num]}">`;
+	document.getElementById("myimg").innerHTML=`<img class="images" src="${images[num]}">`;
 }
 start();
